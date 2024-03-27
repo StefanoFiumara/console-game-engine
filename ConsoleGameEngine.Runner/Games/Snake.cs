ac66793219e1c366ec40261504d8bbc8f6c8e89a
Stefano Fiumara <sdf10517@gmail.com>
2024-03-27 15:48:10 -0500

    snake AI WIP

--- ConsoleGameEngine.Runner/Games/Snake.cs
+++ ConsoleGameEngine.Runner/Games/Snake.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using ConsoleGameEngine.Core;
 using ConsoleGameEngine.Core.GameObjects;
 using ConsoleGameEngine.Core.Input;
@@ -85,11 +87,12 @@ public class Snake : ConsoleGameEngineBase
 
         Fill(ScreenRect, ' ');
 
+        // TODO: create a toggle for different control methods (AI vs manual)
         // Handle Input
-        if(input.IsKeyHeld(KeyCode.Left)  && _snakeDirection != Vector.Right) _input = Vector.Left;
-        if(input.IsKeyHeld(KeyCode.Right) && _snakeDirection != Vector.Left)  _input = Vector.Right;
-        if(input.IsKeyHeld(KeyCode.Up)    && _snakeDirection != Vector.Down)  _input = Vector.Up;
-        if(input.IsKeyHeld(KeyCode.Down)  && _snakeDirection != Vector.Up)    _input = Vector.Down;
+        // if(input.IsKeyHeld(KeyCode.Left)  && _snakeDirection != Vector.Right) _input = Vector.Left;
+        // if(input.IsKeyHeld(KeyCode.Right) && _snakeDirection != Vector.Left)  _input = Vector.Right;
+        // if(input.IsKeyHeld(KeyCode.Up)    && _snakeDirection != Vector.Down)  _input = Vector.Up;
+        // if(input.IsKeyHeld(KeyCode.Down)  && _snakeDirection != Vector.Up)    _input = Vector.Down;
 
         // Ticks the game forward every GAME_TICK seconds.
         _gameTimer -= elapsedTime;
@@ -98,6 +101,12 @@ public class Snake : ConsoleGameEngineBase
             // Game ticks faster based on current level
             _gameTimer = GameTick - _level * 0.02f;
 
+            (_input, var score) = DetermineNextDirection(_head);
+            if (score == int.MinValue)
+            {
+                Debugger.Break();
+            }
+            
             _snakeDirection = _input;
             _head += _snakeDirection;
             _body.Add(_head);
@@ -163,4 +172,69 @@ public class Snake : ConsoleGameEngineBase
 
         return true;
     }
+    
+    private static readonly int LookAheadDepth = 10;
+    private readonly Vector[] _directions = { Vector.Left, Vector.Right, Vector.Up, Vector.Down };
+    private Dictionary<(Vector, Vector, int), int> _scoreMemo = new();
+    
+    private (Vector direction, int score) DetermineNextDirection(Vector startingPos)
+    {
+        _scoreMemo = new Dictionary<(Vector, Vector, int), int>();
+        var scores = new Dictionary<Vector, int>
+        {
+            [Vector.Left] = 0,
+            [Vector.Right] = 0,
+            [Vector.Up] = 0,
+            [Vector.Down] = 0
+        };
+    
+        foreach (var direction in scores.Keys)
+        {
+            scores[direction] = GetScoreForPosition(startingPos, direction, LookAheadDepth);
+        }
+        
+        var maxScore = scores.Values.Max();
+        var nextDirection = scores.First(kvp => kvp.Value == maxScore).Key;
+        return (nextDirection, maxScore);
+    }
+    
+    private int GetScoreForPosition(Vector startingPos, Vector direction, int depth)
+    {
+        var nextPosition = startingPos + direction;
+        if (depth == 0) return 0;
+        
+        if (_scoreMemo.TryGetValue((nextPosition, direction, depth), out int cachedScore))
+        {
+            return cachedScore;
+        }
+    
+        if (_body.Any(b => b == nextPosition) || _map.Sprite.GetGlyph((int)(nextPosition.X - _map.Position.X), (int)(nextPosition.Y - _map.Position.Y)) == Wall) 
+        {
+            return _scoreMemo[(nextPosition, direction, depth)] = int.MinValue;
+        }
+    
+        int score = 1000; // base score for non-collision move
+
+        // Add to score based on closeness to the food
+        var distanceToFood = (int)(_food - nextPosition).Magnitude;
+        score -= distanceToFood * 10;
+
+        if (distanceToFood == 0) score += 1000;
+
+        // Add to score based on availability of next potential moves
+        var freedSpacesAfterMove = _directions.Count(dir => IsSpaceFree(nextPosition + dir));
+        score += freedSpacesAfterMove * 500;
+
+        // Other factors could be added here (like distance to tail)
+
+        // Recur for look-ahead depth
+        var futureScores = _directions.Select(dir => GetScoreForPosition(nextPosition, dir, depth - 1)).Max();
+        return _scoreMemo[(nextPosition, direction, depth)] = score + futureScores;
+    }
+
+    private bool IsSpaceFree(Vector pos)
+    {
+        return _body.All(b => b != pos) && _map.Sprite.GetGlyph((int)(pos.X - _map.Position.X), (int)(pos.Y - _map.Position.Y)) != Wall;
+    }
+
 }
