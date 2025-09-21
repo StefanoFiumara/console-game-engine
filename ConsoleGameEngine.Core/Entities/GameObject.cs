5c3a8275e56c5c857bbe9a681435d26f12838114
Stefano Fiumara <sdfiumara@proton.me>
2025-09-21 15:20:13 -0500

    define button class (wip)

--- ConsoleGameEngine.Core/Entities/GameObject.cs
+++ ConsoleGameEngine.Core/Entities/GameObject.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleGameEngine.Core.Graphics;
+﻿using System;
+using ConsoleGameEngine.Core.Graphics;
 using ConsoleGameEngine.Core.Math;
 
 namespace ConsoleGameEngine.Core.Entities;
@@ -8,4 +9,30 @@ public abstract class GameObject(Vector position = default)
     public Vector Position { get; set; } = position;
     public abstract Rect Bounds { get; }
     public abstract void Draw(IRenderer renderer);
+}
+
+public class Button : GameObject
+{
+    private readonly Action _onClick;
+    public override Rect Bounds => new(Position, _size);
+    
+    public string Label { get; set; }
+    private readonly Vector _size;
+
+    public Button(string label, int width, int height, Action onClick, Vector position = default)
+    {
+        Label = label;
+        Position = position;
+        _onClick = onClick;
+        width = System.Math.Max(label.Length, width);
+        height = System.Math.Max(3, height);
+        _size = new Vector(width, height);
+    }
+    
+    // TODO: How do we listen for clicks? 
+    
+    public override void Draw(IRenderer renderer)
+    {
+        
+    }
 }
